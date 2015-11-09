610d68519c9da9a59c4d0fe4337f8d9227d6ecbd
OlegPalchyk <OlegPa@interlink-ua.com>
2015-11-09 21:46:29 +0200

    Fill all tables id Shop database

--- fileShop.sql
+++ fileShop.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.44, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
 -- Host: localhost    Database: Shop
 -- ------------------------------------------------------
--- Server version	5.5.44-0ubuntu0.14.04.1
+-- Server version	5.7.9-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,13 +16,13 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Category`
+-- Table structure for table `category`
 --
 
-DROP TABLE IF EXISTS `Category`;
+DROP TABLE IF EXISTS `category`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Category` (
+CREATE TABLE `category` (
   `id` int(11) NOT NULL,
   `Title` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -30,23 +30,23 @@ CREATE TABLE `Category` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Category`
+-- Dumping data for table `category`
 --
 
-LOCK TABLES `Category` WRITE;
-/*!40000 ALTER TABLE `Category` DISABLE KEYS */;
-INSERT INTO `Category` VALUES (1,'Water'),(2,'Fruits'),(3,'Vegetables'),(4,'Tea'),(5,'Milk Products');
-/*!40000 ALTER TABLE `Category` ENABLE KEYS */;
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Water'),(2,'Fruits'),(3,'Vegetables'),(4,'Tea'),(5,'Milk Products');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Customer`
+-- Table structure for table `customer`
 --
 
-DROP TABLE IF EXISTS `Customer`;
+DROP TABLE IF EXISTS `customer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Customer` (
+CREATE TABLE `customer` (
   `id` int(11) NOT NULL,
   `Name` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -54,93 +54,94 @@ CREATE TABLE `Customer` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Customer`
+-- Dumping data for table `customer`
 --
 
-LOCK TABLES `Customer` WRITE;
-/*!40000 ALTER TABLE `Customer` DISABLE KEYS */;
-INSERT INTO `Customer` VALUES (1,'John'),(2,'Jack'),(3,'Anna'),(4,'Tony'),(5,'Stive');
-/*!40000 ALTER TABLE `Customer` ENABLE KEYS */;
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES (1,'John'),(2,'Jack'),(3,'Anna'),(4,'Tony'),(5,'Stive');
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Item`
+-- Table structure for table `item`
 --
 
-DROP TABLE IF EXISTS `Item`;
+DROP TABLE IF EXISTS `item`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Item` (
+CREATE TABLE `item` (
   `Product_id` int(11) DEFAULT NULL,
   `Quantity` int(11) DEFAULT NULL,
   `Order_id` int(11) DEFAULT NULL,
   KEY `Order_id` (`Order_id`),
-  CONSTRAINT `Item_ibfk_1` FOREIGN KEY (`Order_id`) REFERENCES `OrderTable` (`id`)
+  CONSTRAINT `Item_ibfk_1` FOREIGN KEY (`Order_id`) REFERENCES `ordertable` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Item`
+-- Dumping data for table `item`
 --
 
-LOCK TABLES `Item` WRITE;
-/*!40000 ALTER TABLE `Item` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Item` ENABLE KEYS */;
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (1,2,1),(4,2,1),(20,10,2);
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `OrderTable`
+-- Table structure for table `ordertable`
 --
 
-DROP TABLE IF EXISTS `OrderTable`;
+DROP TABLE IF EXISTS `ordertable`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `OrderTable` (
+CREATE TABLE `ordertable` (
   `id` int(11) NOT NULL DEFAULT '0',
   `Customer_id` int(11) DEFAULT NULL,
   `Date` date DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_perCustomer` (`Customer_id`),
-  CONSTRAINT `fk_perCustomer` FOREIGN KEY (`Customer_id`) REFERENCES `Customer` (`id`)
+  CONSTRAINT `fk_perCustomer` FOREIGN KEY (`Customer_id`) REFERENCES `customer` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `OrderTable`
+-- Dumping data for table `ordertable`
 --
 
-LOCK TABLES `OrderTable` WRITE;
-/*!40000 ALTER TABLE `OrderTable` DISABLE KEYS */;
-INSERT INTO `OrderTable` VALUES (1,1,'2015-11-11'),(2,1,'2015-11-12'),(3,2,'2015-11-13'),(4,3,'2015-11-14'),(5,4,'2015-11-20'),(6,5,'2015-11-30'),(7,5,'2015-11-30');
-/*!40000 ALTER TABLE `OrderTable` ENABLE KEYS */;
+LOCK TABLES `ordertable` WRITE;
+/*!40000 ALTER TABLE `ordertable` DISABLE KEYS */;
+INSERT INTO `ordertable` VALUES (1,1,'2015-11-11'),(2,1,'2015-11-12'),(3,2,'2015-11-13'),(4,3,'2015-11-14'),(5,4,'2015-11-20'),(6,5,'2015-11-30'),(7,5,'2015-11-30');
+/*!40000 ALTER TABLE `ordertable` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Product`
+-- Table structure for table `product`
 --
 
-DROP TABLE IF EXISTS `Product`;
+DROP TABLE IF EXISTS `product`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Product` (
+CREATE TABLE `product` (
   `id` int(11) NOT NULL,
   `Title` varchar(20) DEFAULT NULL,
   `Price` decimal(10,0) DEFAULT NULL,
   `Category_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_perCategory` (`Category_id`),
-  CONSTRAINT `fk_perCategory` FOREIGN KEY (`Category_id`) REFERENCES `Category` (`id`)
+  CONSTRAINT `fk_perCategory` FOREIGN KEY (`Category_id`) REFERENCES `category` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Product`
+-- Dumping data for table `product`
 --
 
-LOCK TABLES `Product` WRITE;
-/*!40000 ALTER TABLE `Product` DISABLE KEYS */;
-INSERT INTO `Product` VALUES (1,'Sprite',10,1),(2,'Coca-cola',10,1),(3,'Fanta',10,1),(4,'Pepsi',10,1),(5,'Zhuvchik',11,1),(6,'Apple',3,2),(7,'Mango',4,2),(8,'Pear',5,2),(9,'Cherry',6,2),(10,'PineApple',10,2),(11,'Potato',1,3),(12,'Tomato',2,3),(13,'Cabbage',4,3),(14,'Onion',1,3),(15,'Garlic',1,3),(16,'Lipton',12,4),(17,'Tess',16,4),(18,'Princess Nury',10,4),(19,'Tseilon',10,4),(20,'Milk',8,5),(21,'Cheese',8,5),(22,'Cottage cheese',8,5),(23,'Yogurt',20,5);
-/*!40000 ALTER TABLE `Product` ENABLE KEYS */;
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'Sprite',10,1),(2,'Coca-cola',10,1),(3,'Fanta',10,1),(4,'Pepsi',10,1),(5,'Zhuvchik',11,1),(6,'Apple',3,2),(7,'Mango',4,2),(8,'Pear',5,2),(9,'Cherry',6,2),(10,'PineApple',10,2),(11,'Potato',1,3),(12,'Tomato',2,3),(13,'Cabbage',4,3),(14,'Onion',1,3),(15,'Garlic',1,3),(16,'Lipton',12,4),(17,'Tess',16,4),(18,'Princess Nury',10,4),(19,'Tseilon',10,4),(20,'Milk',8,5),(21,'Cheese',8,5),(22,'Cottage cheese',8,5),(23,'Yogurt',20,5);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -152,4 +153,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-11-09 18:03:51
+-- Dump completed on 2015-11-09 21:44:47
