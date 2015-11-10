c6147098f243fdab89909d776405b743280f70ad
OlegPalchyk <olegpa@interlink-ua.com>
2015-11-10 10:09:40 +0200

    Add procedurec for 1 and 4 items at table

--- Query.sql
+++ Query.sql
@@ -2,4 +2,43 @@ create procedure `addCustomer`(IN paramName varchar(10))
  MODIFIES SQL DATA
     BEGIN
    insert into customer (Name) values (paramName);
-    END
+    END//
+
+create procedure `addCategory` (IN paramTitle varchar(10))
+MODIFIES SQL DATA
+BEGIN
+insert into category (Title) values (paramTitle);
+END //
+
+create procedure `addProduct` (IN paramTitle varchar(10), IN paramPrice DECIMAL, IN paramCategory INT)
+MODIFIES SQL DATA
+BEGIN
+insert into product (Title, Price, Category_id) values (paramTitle, paramPrice, paramCategory);
+END //
+
+create procedure `addItem` (IN paramProduct INT, IN paramQuantity INT, IN paramOrder INT)
+MODIFIES SQL DATA
+BEGIN
+insert into item(Product_id, Quantity, Order_id) values (paramProduct, paramQuantity, paramOrder);
+END//
+
+create procedure `addOrderItem` ( IN paramCustomer INT, IN paramDate DATE)
+MODIFIES SQL DATA
+BEGIN
+insert into ordertable (Customer_id, Date) values (paramCustomer, paramDate);
+END //
+
+
+create procedure `getProductByCategory` (IN paramCategory varchar(10))
+MODIFIES SQL DATA
+BEGIN
+select product.Title FROM category JOIN product where category.id=product.Category_id AND category.Title LIKE paramCategory;
+END //
+
+create procedure `getOrderByCusomer` (IN paramCustomer varchar(10))
+MODIFIES SQL DATA
+BEGIN
+select * from customer c, ordertable o WHERE (c.id=o.Customer_id) AND (c.Name = paramCustomer);
+END //
+
+
---
+++ Query.sql~
@@ -0,0 +1,5 @@
+create procedure `addCustomer`(IN paramName varchar(10))
+ MODIFIES SQL DATA
+    BEGIN
+   insert into customer (Name) values (paramName);
+    END
